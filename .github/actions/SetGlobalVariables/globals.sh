7ac1f822493477ce6db18b3b696ad9027c84c574
InterviewProOrg <168927421+InterviewProOrg@users.noreply.github.com>
2024-12-28 22:36:30 +0530

    Update globals.sh

--- .github/actions/SetGlobalVariables/globals.sh
+++ .github/actions/SetGlobalVariables/globals.sh
@@ -1,4 +1,3 @@
-echo "RepositoryOwner=${{ github.repository_owner }}" >> $GITHUB_OUTPUT
 echo "DefaultBranch=main" >> $GITHUB_OUTPUT
 echo "CICDKey=InterviewProCICDKey" >> $GITHUB_OUTPUT
 echo "EmailRecipient=dev@test.com" >> $GITHUB_OUTPUT
